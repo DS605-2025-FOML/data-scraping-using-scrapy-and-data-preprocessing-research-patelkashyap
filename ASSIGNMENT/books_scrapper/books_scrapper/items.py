f6a03273a977484f947465f85dba40575df99d58
Kashyap Patel <kashyappatel@Kashyaps-MacBook-Pro.local>
2025-08-08 17:52:23 +0530

    Completed assignment. Collected Book title, price, rating and stock availability.

---
+++ ASSIGNMENT/books_scrapper/books_scrapper/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BooksScrapperItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # pass
+    title = scrapy.Field()
+    price = scrapy.Field()
+    rating = scrapy.Field()
+    stock_availability = scrapy.Field()
---
+++ ASSIGNMENT/books_scrapper/books_scrapper/spiders/books_spider.py
@@ -0,0 +1,36 @@
+import scrapy
+from ..items import BooksScrapperItem
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    start_urls = [
+        'https://books.toscrape.com/'
+    ]
+    
+    def parse(self, response):
+        all_article_books = response.css("article.product_pod")
+        items = BooksScrapperItem()
+        
+        for book_article in all_article_books:
+            title = book_article.css('h3 a::text').extract_first()
+            
+            price = book_article.css('div.product_price p.price_color::text').extract_first()
+            
+            rating = book_article.css('p.star-rating::attr(class)').extract()
+            rating = rating[-1]
+            rating = rating.split()[-1]
+            
+            stock_availability = book_article.css('div.product_price > p:nth-of-type(2)::text').extract()
+            stock_availability = ''.join(stock_availability).strip()
+            
+            items['title'] = title
+            items['price'] = price
+            items['rating'] = rating
+            items['stock_availability'] = stock_availability
+            
+            yield items
+            
+        next_page = response.css('li.next a::attr(href)').get()
+        
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
